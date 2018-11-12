d2a52d7e4901e399d4a19e61a92fa7132da23655
Tobias Feld <tobias.feld@hotmail.com>
2018-11-12 16:44:46 +0100

    Change permissions

--- permissions/crew.js
+++ permissions/crew.js
@@ -1,8 +1,8 @@
 const spark = require("sparkbots")
 const perm = spark.permission("crew")
 perm.setLevel(5)
-perm.code = (client) => {
-    if (client.guilds.get("375636201648160768").roles.has("489484578734473237")) {
+perm.code = (client, message) => {
+    if (message.guild.id == "375636201648160768" && message.guild.roles.has("489484578734473237")){
         return false
     }
     return true
--- permissions/fa.js
+++ permissions/fa.js
@@ -1,11 +1,12 @@
 const spark = require("sparkbots")
 const perm = spark.permission("fa")
 perm.setLevel(4)
-perm.code = (client) => {
+perm.code = (client, message) => {
 
-    if (client.guilds.get("375636201648160768").roles.has("489484578734473237") || client.guilds.get("375636201648160768").roles.has(375694610233819136)) {
+    if (message.guild.id == "375636201648160768" && (message.guild.roles.has("489484578734473237") || message.guild.roles.has("375694610233819136"))) {
         return false
     }
     return true
 }
+
 module.exports = perm
