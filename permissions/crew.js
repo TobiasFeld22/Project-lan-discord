c979876f504226e2c77691dc6426b1884e2417c5
Tobias Feld <tobias.feld@hotmail.com>
2018-11-13 21:40:41 +0100

    Fix perms

--- permissions/crew.js
+++ permissions/crew.js
@@ -2,7 +2,8 @@ const spark = require("sparkbots")
 const perm = spark.permission("crew")
 perm.setLevel(5)
 perm.code = (client, message) => {
-    if (message.guild.id == "375636201648160768" && message.guild.roles.has("489484578734473237")){
+    if (!message.guild){return true}
+    if (message.guild.id == "375636201648160768" && message.user.roles.has("489484578734473237")){
         return false
     }
     return true
--- permissions/fa.js
+++ permissions/fa.js
@@ -2,8 +2,8 @@ const spark = require("sparkbots")
 const perm = spark.permission("fa")
 perm.setLevel(4)
 perm.code = (client, message) => {
-
-    if (message.guild.id == "375636201648160768" && (message.guild.roles.has("489484578734473237") || message.guild.roles.has("375694610233819136"))) {
+    if (!message.guild){return true}
+    if (message.guild.id == "375636201648160768" && (message.user.roles.has("489484578734473237") || message.user.roles.has("375694610233819136"))) {
         return false
     }
     return true
