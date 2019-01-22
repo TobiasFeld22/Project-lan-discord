1ffc9bd3ba6e599fd87c21f30893aa502688a995
Tobias Feld <tobias.feld@hotmail.com>
2019-01-22 01:23:00 +0100

    Update aanwezig command voor de gamebattle.

--- commands/aanwezig.js
+++ commands/aanwezig.js
@@ -8,19 +8,19 @@ command.code = (client, message) => {
     if (message.guild.id != "375636201648160768") {
         return message.channel.send("Dit command werkt alleen in de **Project:LAN** server.")
     }
-    if (message.member.roles.has("502811101365272587")) {
-        return message.member.removeRole("502811101365272587")
+    if (message.member.roles.has("537062265044598787")) {
+        return message.member.removeRole("537062265044598787")
             .then(() => {
-                message.channel.send("Ik heb de **Aanwezig CLV-LAN 31** role weggehaald.")
+                message.channel.send("Ik heb de **Aanwezig GameBattle 2019** role weggehaald.")
             })
             .catch(e => {
                 console.error(e)
                 message.channel.send(":x: Er is een fout opgetreden.\n Probeer het later opnieuw")
             })
     }
-    return message.member.addRole("502811101365272587")
+    return message.member.addRole("537062265044598787")
         .then(() => {
-            message.channel.send("Leuk dat je naar CLV-LAN 31 komt!\n\nIk heb je de **Aanwezig CLV-LAN 31** role gegeven.")
+            message.channel.send("Leuk dat je naar de Gamebattle 2019 komt!\n\nIk heb je de **Aanwezig GameBattle 2019** role gegeven.")
         })
         .catch(e => {
             console.error(e)
