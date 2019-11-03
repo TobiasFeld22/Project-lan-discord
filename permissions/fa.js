3065de3fae868db135626abc45256eb981716031
Tobias Feld <tobias.feld@hotmail.com>
2019-11-03 22:44:25 +0100

    Regel permissies met nieuwe crew / fa rollen.

--- permissions/fa.js
+++ permissions/fa.js
@@ -1,9 +1,17 @@
+/* eslint no-underscore-dangle: off */
 const spark = require("sparkbots")
 const perm = spark.permission("fa")
 perm.setLevel(4)
+const roles = [
+    "637764396818366464",
+    "489480374678978560",
+    "637763172555554818",
+    "637763310321664010",
+    "489483634571739166"
+]
 perm.code = (client, message) => {
-    if (!message.guild){return true}
-    if (message.guild.id == "375636201648160768" && (message.member.roles.has("489484578734473237") || message.member.roles.has("375694610233819136"))) {
+    if (!message.guild) {return true}
+    if (message.guild.id == "375636201648160768" && (roles.filter(value => message.member._roles.indexOf(value) !== -1)).length > 0) {
         return false
     }
     return true
